55c22f6badac54e38e1eb15506460d1792e9d97a
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-16 12:42:51 -0500

    link js file and handle initial favorites list

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,1 +1,35 @@
 console.log('working')
+const teamList = document.querySelector('.team-list')
+const input = document.querySelector('.team-input')
+const addButton = document.querySelector('.add-button')
+
+const initialize = () => {
+  addButton.addEventListener('click', e => {
+    e.preventDefault()
+    handleInput()
+  })
+}
+
+let favorites = {
+  teams: []
+}
+
+const updateList = (arr, list) => {
+  list.childNodes.forEach(child => {
+    list.removeChild(list.childNodes[0])
+  })
+  arr.forEach(item => {
+    let listItem = document.createElement('li')
+    listItem.innerText = item
+    list.appendChild(listItem)
+  })
+}
+
+const handleInput = () => {
+  let newTeam = input.value
+  favorites.teams.push(newTeam)
+  updateList(favorites.teams, teamList)
+  input.value = ''
+}
+
+initialize()
