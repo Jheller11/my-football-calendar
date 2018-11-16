a5a08ee09eff52d88c8f3c0073e7991a2e91ed8e
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-11-16 15:56:44 -0500

    config xhttp request on reload calendar

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -2,11 +2,27 @@ console.log('working')
 const teamList = document.querySelector('.team-list')
 const input = document.querySelector('.team-input')
 const addButton = document.querySelector('.add-button')
+const refreshButton = document.querySelector('.refresh-button')
+const calendarList = document.querySelector('.calendar-list')
 
 const initialize = () => {
   addButton.addEventListener('click', e => {
     e.preventDefault()
     handleInput()
+  })
+  refreshButton.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('clicked')
+    let xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        let data = JSON.parse(xhttp.response)
+        console.log(data)
+        updateCalendar(data, calendarList)
+      }
+    }
+    xhttp.open('GET', 'http://localhost:4000/calendar/refresh', true)
+    xhttp.send()
   })
 }
 
@@ -15,6 +31,17 @@ let favorites = {
 }
 
 const updateList = (arr, list) => {
+  list.childNodes.forEach(child => {
+    list.removeChild(list.childNodes[0])
+  })
+  arr.forEach(item => {
+    let listItem = document.createElement('li')
+    listItem.innerText = item
+    list.appendChild(listItem)
+  })
+}
+
+const updateCalendar = (arr, list) => {
   list.childNodes.forEach(child => {
     list.removeChild(list.childNodes[0])
   })
--- routers/calendar.js
+++ routers/calendar.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+// ajax request for refreshing calendar
+router.get('/refresh', (req, res) => {
+  res.json(['1', '2'])
+})
+
+// get calendar
 router.get('/', (req, res) => {
   res.render('calendar')
 })
